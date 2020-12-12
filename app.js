44e3bd02ab566c44938177abb7d17cf5b20a5e4c
Brandon Rupert <brupert94k@gmail.com>
2020-12-12 02:37:42 -0600

    added url creation function

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 
 const apiKey = 'OiONWIfvr0i94cSwrGaZh5d3Yq0746UbbbHD8hoh';
 
-const baseUrl = 'developer.nps.gov/api/v1';
+const baseUrl = 'https://developer.nps.gov/api/v1/parks';
+
+
+function formatUrlParams(params) {
+    const queryItems = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        
+    return queryItems.join('&');
+
+}
 
 
 
@@ -24,15 +33,39 @@ const baseUrl = 'developer.nps.gov/api/v1';
 
 
 
+function retrieveList(){
+    const params = {
+        api_key : apiKey,
+        parkCode: document.getElementById('state').value,
+        
+    };
+    
+    const queryString = formatUrlParams(params);
+    const url = baseUrl + '?' + queryString;
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    response.json();
+                }
+                    throw new Error(response.status);
+            
+            })
+            .then(responseJson => showCampList(responseJson))
+            .catch(err => {
+                $('.error-message').show();
+                })
 
+ 
 
-
-
+};
 
 
 
 function getCampListings(){
-
+    $('main').submit(event => {
+        event.preventDefault();
+        retrieveList();
+    })
 };
 
 
