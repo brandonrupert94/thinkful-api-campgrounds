ed937054a8a929f268d1b58aaa75861be6939aa5
Brandon Rupert <brupert94k@gmail.com>
2020-12-12 01:28:21 -0600

    added api key and base url

---
+++ app.js
@@ -0,0 +1,48 @@
+
+const apiKey = 'OiONWIfvr0i94cSwrGaZh5d3Yq0746UbbbHD8hoh';
+
+const baseUrl = 'developer.nps.gov/api/v1';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getCampListings(){
+
+};
+
+
+
+
+
+
+function handleCampApp(){
+    console.log('app is ready to run')
+    getCampListings();
+}
+
+$(handleCampApp());
